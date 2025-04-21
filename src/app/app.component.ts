1d21e38921eb115e7fac01be631c3f9071a03671
maikonmuniz <maikon.muniz@yahoo.com.br>
2025-04-20 23:13:11 -0300

    feat: create router link toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { FormComponent } from './form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "register", component: RegisterComponent},
+    {path: "form", component: FormComponent}
+];
